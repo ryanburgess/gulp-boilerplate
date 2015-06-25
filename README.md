3c5c732689f6ba59691a2d1916d49f16febfcded
ryanburgess <ryanb@netflix.com>
2015-06-24 17:42:27 -0700

    add support for travis ci

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-Gulp Boilerplate
-=============
+# Gulp Boilerplate
+
+[![npm version](https://badge.fury.io/js/gulp-boilerplate.svg)](http://badge.fury.io/js/gulp-boilerplate) [![Build Status](https://travis-ci.org/ryanburgess/gulp-boilerplate.svg?branch=master)](https://travis-ci.org/ryanburgess/gulp-boilerplate)
+
 A gulp boilerplate to start your next project.
 
 Gulp plugins included in the boilerplate:
@@ -21,6 +23,7 @@ gulp-boilerplate
 ```
 
 ## Release History
+* 1.1.1: add Travis CI testing.
 * 1.1.0: Update browserify to build all files in root JS.
 * 1.0.8: Update documentation.
 * 1.0.7: Automatically install npm packages.
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp        = require('gulp'),
   minifyCSS     = require('gulp-minify-css'),
   sass          = require('gulp-sass'),
   browserify    = require('gulp-browserify'),
-  browserSync   = require('browser-sync'),
   uglify        = require('gulp-uglify'),
   rename        = require('gulp-rename'),
   jshint        = require('gulp-jshint'),
@@ -10,25 +9,6 @@ var gulp        = require('gulp'),
   replace       = require('gulp-replace'),
   notify        = require('gulp-notify'),
   path          = require('path');
-
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: path.join(__dirname, 'build')
-    },
-    host: 'localhost',
-    port: 1337,
-    open: false
-  });
-});
-
-gulp.task('html', function(){
-  gulp.src(path.join(__dirname, 'views/**/*.html'))
-    .pipe(gulp.dest(path.join(__dirname, 'build')))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
 
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
@@ -39,7 +19,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('jshint', function() {
-  return gulp.src('./js/**/*.js')
+  return gulp.src(['./js/**/*.js', './bin/*'])
     .pipe(jshint())
     .pipe(jshint.reporter(jshintStyle))
     .pipe(jshint.reporter('fail'))
@@ -62,10 +42,11 @@ gulp.task('scripts', function() {
     .pipe(notify({ message: 'JS files complete' }));
 });
 
-gulp.task('watch', ['browser-sync'], function() {
+gulp.task('watch', function() {
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('js/**/*.js', ['jshint', 'scripts']);
   gulp.watch('views/**/*.html', ['html']);
 });
 
+gulp.task('test', ['jshint']);
 gulp.task('default', ['watch']);
