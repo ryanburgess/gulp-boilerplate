8b5dfda2dae641c2270716737f61af912476ab39
ryanburgess <burgessdryan@gmail.com>
2017-08-28 15:24:37 -0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,77 @@
-var gulp        = require('gulp'),
-  minifyCSS     = require('gulp-minify-css'),
-  sass          = require('gulp-sass'),
-  browserify    = require('gulp-browserify'),
-  uglify        = require('gulp-uglify'),
-  nodeunit      = require('gulp-nodeunit'),
-  rename        = require('gulp-rename'),
-  jshint        = require('gulp-jshint'),
-  jshintStyle   = require('jshint-stylish'),
-  replace       = require('gulp-replace'),
-  notify        = require('gulp-notify'),
-  path          = require('path');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-minify-css');
+const jshintStyle = require('jshint-stylish');
+const eslint = require('gulp-eslint');
+const browserify = require('browserify');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const source = require('vinyl-source-stream');
 
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./build/css/'))
-    .pipe(notify({ message: 'CSS complete' }));
+    .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('jshint', function() {
-  return gulp.src(['./js/**/*.js', './bin/*'])
-    .pipe(jshint())
-    .pipe(jshint.reporter(jshintStyle))
-    .pipe(jshint.reporter('fail'))
-    .pipe(notify({ message: 'JSHint complete' }));
+// --------------------------------------------------------------
+//  ESLint
+// --------------------------------------------------------------
+gulp.task('lint', function () {
+  // lint all JS in all project directories except node_modules and public/js
+  return gulp.src(['**/*.js', '!node_modules/**', '!public/js/**/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
 });
 
-gulp.task('scripts', function() {
-  gulp.src('js/*.js')
-    .pipe(browserify({
-      insertGlobals : true,
-      debug : !gulp.env.production
-    }))
-    .pipe(gulp.dest('./build/js/'))
+// --------------------------------------------------------------
+//  build javascript bundles
+// --------------------------------------------------------------
+gulp.task('javascript', function() {
+ const jsxPath = './jsx/';
+ const files = ['./js/'];
+ const streams = files.map(function(fileName) {
+  const fullFile = jsxPath + fileName + '.jsx';
+  const bundler = browserify({
+    extensions: ['.js', '.jsx'],
+    transform: ['babelify']
+  });
+
+  bundler.add(fullFile);
+
+  const stream = bundler.bundle();
+  stream.on('error', function (err) { console.error(err.toString()); });
+
+  stream
+    .pipe(source(fullFile))
+    .pipe(rename(fileName + '.js'))
+    .pipe(gulp.dest('public/js/'));
+  console.log(`${fileName}.js created`)
+ });
+  
+});
+
+// --------------------------------------------------------------
+//  Compress/minify JavaScript
+// --------------------------------------------------------------
+gulp.task('compress', function() {
+  return gulp.src('./public/js/*.js')
     .pipe(uglify())
     .pipe(rename({
-	     extname: '.min.js'
-	   }))
-    .pipe(replace('./build/js/*.min.js'))
-    .pipe(gulp.dest('./build/js'))
-    .pipe(notify({ message: 'JS files complete' }));
+       extname: '.min.js'
+     }))
+    .pipe(gulp.dest('./public/js/min/'));
 });
 
+// --------------------------------------------------------------
+//  Watch
+// --------------------------------------------------------------
 gulp.task('watch', function() {
   gulp.watch('sass/**/*.scss', ['sass']);
-  gulp.watch('js/**/*.js', ['jshint', 'scripts']);
-  gulp.watch('views/**/*.html', ['html']);
+  gulp.watch(['./jsx/**/*'], ['lint', 'javascript', 'compress']);
 });
 
-// nodeunit tests
-gulp.task('nodeunit', function () {
-  gulp.src('test/**/*.js')
-    .pipe(nodeunit({
-      reporter: 'junit'
-    }));
-});
-
-gulp.task('test', ['jshint', 'nodeunit']);
+// default task is watch
 gulp.task('default', ['watch']);
